ac138b3c910eb204947ad866e56ceeb341284379
FacundoLavagnino1993 <facu9396@gmail.com>
2016-04-27 11:44:17 -0300

    parcial

---
+++ HabemusPapaModeloPrimerParical/HabemusPapaModeloPrimerParical/Cardenal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabemusPapaModeloPrimerParical
+{
+    class Cardenal
+    {
+        private int _cantidadDeVotosRecibidos;
+        private ENacionalidades _nacionalidad;
+        private string _nombre;
+        private string _nombrePapal;
+
+        private Cardenal()
+        {
+            this._cantidadDeVotosRecibidos = 0;
+        }
+
+        public Cardenal(string nombre, string nombrePapal):this()
+        {
+            this._nombre = nombre;
+            this._nombrePapal = nombrePapal;
+        }
+
+        public Cardenal(string nombre, string nombrePapal, ENacionalidades nacionalidad):this(nombre,nombrePapal)
+        {
+            this._nacionalidad = nacionalidad;
+
+        }
+
+        public int getCantidadDeVotosRecibidos()
+        {
+            return this._cantidadDeVotosRecibidos;
+        }
+
+        private string Mostrar()
+        {
+            string respuesta;
+            respuesta = string.Concat(ObtenerNombreYNombrePapal() + " Nacionalidad : " + this._nacionalidad + " Cantidad de votos recibidos : " + this._cantidadDeVotosRecibidos);
+            return respuesta;
+        }
+
+        public static string Mostrar(Cardenal c)
+        {
+            return c.Mostrar();
+        }
+
+        public string ObtenerNombreYNombrePapal()
+        {
+            string alias;
+            alias = string.Concat("El cardenal " + this._nombre + " se llamara " + this._nombrePapal);
+            return alias;
+        }
+
+        public static bool operator !=(Cardenal c1, Cardenal c2)
+        {
+        }
+
+        public static Cardenal operator ++(Cardenal c)
+        {
+        }
+
+        public static bool operator ==(Cardenal c1, Cardenal c2)
+        {
+        }
+
+
+    }
+
+    enum ENacionalidades
+    {
+        Italiano, Polaco, Español, Argentino, Nigeriano
+    }
+}
---
+++ HabemusPapaModeloPrimerParical/HabemusPapaModeloPrimerParical/Conclave.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabemusPapaModeloPrimerParical
+{
+    class Conclave
+    {
+        private int _cantMaxCardenales;
+        private List<Cardenal> _cardenales;
+        private bool _habemusPapa;
+        private string _lugarEleccion;
+        private Cardenal _papa;
+        public static int _cantidadVotaciones;
+        public static DateTime fechaVotacion;
+        
+
+        static Conclave()
+        { 
+            _cantidadVotaciones = 0;
+            fechaVotacion = DateTime.Now;
+        }
+
+        public Conclave()
+        { 
+            this._cantMaxCardenales = 1;
+            this._lugarEleccion = "Capilla Sixtina";
+            this._cardenales = new List<Cardenal>();
+
+        }
+
+        private  Conclave(int cantidadCardenales):this()
+        {
+            
+        }
+
+        public Conclave(int cantidadCardenales, string lugarEleccion):this()
+        {
+            
+
+        }
+
+        private void ContarVotos(Conclave conclave)
+        {
+
+        }
+
+        public static explicit operator bool(Conclave con)
+        {
+            bool hayPapa = false;
+            if (con._habemusPapa == true)
+            {
+                hayPapa = true;
+            }
+
+            return hayPapa;
+        }
+
+        private bool HayLugar()
+        {
+            bool respuesta = false;
+            if (this._cantMaxCardenales > this._cardenales.Count)
+            {
+                respuesta = true;
+            }
+             return respuesta;
+
+        }
+
+        public implicit operator Conclave(int cantidadCardenales)
+        {
+             return new Conclave(cantidadCardenales);
+        }
+
+        public string Mostrar()
+        {
+            string respuesta = "";
+
+            if (this._habemusPapa == true)
+            {
+                respuesta = string.Concat("Lugar de votacion : " + this._lugarEleccion + "\nFecha de Votacion: " + fechaVotacion + "\nCantidad de votos: " + _cantidadVotaciones + "\nHABEMUS PAPA!!!" + this._papa.ObtenerNombreYNombrePapal());
+            }
+            else
+            {
+                respuesta = string.Concat("Lugar de votacion : " + this._lugarEleccion + "\nFecha de Votacion: " + fechaVotacion + "\nCantidad de votos: " + _cantidadVotaciones + "\nNO HABEMUS PAPA!!!" + MostrarCardenales());
+            }
+            
+            
+        }
+
+        private string MostrarCardenales()
+        {
+            string respuesta = "";
+
+            foreach(Cardenal item in _cardenales)
+            {
+                respuesta=Cardenal.Mostrar(item);
+            }
+            return respuesta;
+            
+        }
+
+        public static bool operator !=(Conclave con,Cardenal c)
+        {
+        }
+
+        public static Conclave operator +(Conclave con, Cardenal c)
+        {
+        }
+
+        public static bool operator ==(Conclave con, Cardenal c)
+        {
+        }
+
+        public void VotarPapa(Conclave conclave)
+        {
+        }
+
+
+
+    }
+}
